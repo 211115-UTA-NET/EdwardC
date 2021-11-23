8b5e2e292e8d4913244a2d60da438599c64ed00f
Edward cicio <edwardcicioc@gmail.com>
2021-11-23 14:55:10 -0500

    myFunction

---
+++ C-Sharp_Programs/wk2/Functions/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static void Main(){
+
+    // Main 
+
+    for( int counterForTheForLoop = 0; counterForTheForLoop < 5; counterForTheForLoop++)
+    {
+        int returnedValueFromSayHello = SayHello();
+
+        Console.WriteLine(returnedValueFromSayHello);
+    }
+
+    int a, b;
+    a = 2;
+    b =4;
+
+    int c;
+    c = NewFunc(a, b);                  //call the function, sent variables
+
+    Console.WriteLine(c);
+
+    c = NewFunc(5, 6);                  // call the function, sent values
+    Console.WriteLine(c);
+
+    Console.WriteLine(NewFunc(1, 4));   // return function value to output
+
+    c = Multiply(5, 6);
+    Console.WriteLine(c);
+
+}
+
+static int NewFunc(int x, int y){
+    int z;
+    z = x + y;
+    return z;
+
+    //return x + y;
+}
+
+static int Multiply(int x, int y){
+    int z;
+    z = x * y;
+    return z;
+
+    //return x * y;
+}
+
+static int SayHello(){
+    Console.WriteLine("Hi There!");
+    int j = 11;
+    return j;
+}
+
+Main();
